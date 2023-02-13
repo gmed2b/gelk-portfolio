238c13e6c9533480435cec10447c18eaa1e11001
Mehdi GELK <medghouly@gmail.com>
2023-02-13 17:17:11 +0000

    fix(meta): update website info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Portfolio - Mehdi Ghoulam'
+export const SITE_DESCRIPTION = 'Personal portfolio built with Astro and React.'
